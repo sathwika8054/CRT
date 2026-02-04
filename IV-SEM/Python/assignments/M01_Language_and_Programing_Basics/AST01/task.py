88dcf331589774ca753fde37038ff90fe4e366ef
Sathwika <sathwika638@gmail.com>
2026-02-04 22:37:34 +0530

    msg

--- IV-SEM/Python/assignments/M01_Language_and_Programing_Basics/AST01/task.py
+++ IV-SEM/Python/assignments/M01_Language_and_Programing_Basics/AST01/task.py
@@ -1,4 +1,12 @@
 def Ticket_Pricing(n: int) -> int:
+   if n<=5:
+      return 0
+   elif 6<=n and n<=17:
+      return 10
+   elif 18<=n and n<=64:
+      return 20
+   elif n>=65:
+      return 15
    pass
 
 

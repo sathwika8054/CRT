e9848209682636bf1a6e7eee2e27b4651b7fe6f6
Sathwika <sathwika638@gmail.com>
2026-02-09 14:40:29 +0530

    day_09

--- IV-SEM/Python/Practice/M01_Language_and_Programming_Basics/S04/PS07_Array_basic.py
+++ IV-SEM/Python/Practice/M01_Language_and_Programming_Basics/S04/PS07_Array_basic.py
@@ -1,4 +1,4 @@
-import array
+'''import array
 arr=array.array('i',[])
 print(arr,type(arr))
 arr.append(10)
@@ -6,12 +6,12 @@ arr.append(30)
 arr.append(44)
 arr.append(34455)
 print(arr)
-'''List:
+List:
 1. use [] to create a list
 2.list is Mutable
 3.list allows duplicates
 4.list is heterogeous 
-5.list is indexed'''
+5.list is indexed
 li=[1,2,3,4,5,6,7,"A",'Hello','World',22333.33]
 print(li,type(li))
 print(li[3])#using specific index
@@ -27,4 +27,19 @@ li.count(1)
 print(li)
 "read a number from the user and display no.of digits in the number"
 n=int(input())
-print(len(str(n)))
+print(len(str(n)))'''
+import numpy as np
+
+arr=np.array([10,20,30,40,50])
+print("original array:",arr)
+print(np.max(arr))
+print(np.min(arr))
+print(np.mean(arr))
+print(np.sum(arr))
+print(np.zeros(5))
+print(np.ones(8))
+print("Even numbers are:",np.arange(2,10,2))
+print("Odd numbers are:",np.arange(1,10,2))
+n=int(input("Enter the size"))
+ele=list(map(int,input("Enter the elements:").split()))
+print("Array ")
